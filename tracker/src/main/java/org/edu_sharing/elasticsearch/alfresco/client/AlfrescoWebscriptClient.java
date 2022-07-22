4ba7129ace2e12df011c87d962d492b7bab95f29
wollner <wollner@edu-sharing.net>
2022-07-22 14:47:34 +0200

    optimization: filter metadata requests by type

--- tracker/src/main/java/org/edu_sharing/elasticsearch/alfresco/client/AlfrescoWebscriptClient.java
+++ tracker/src/main/java/org/edu_sharing/elasticsearch/alfresco/client/AlfrescoWebscriptClient.java
@@ -49,7 +49,7 @@ public class AlfrescoWebscriptClient {
 
     String URL_NODE_METADATA_UUID = "/alfresco/s/api/solr/metadata/uuid?uuid={{uuid}}";
 
-    String URL_ACL_READERS ="/alfresco/s/api/solr/aclsReaders";
+    String URL_ACL_READERS = "/alfresco/s/api/solr/aclsReaders";
 
     String URL_ACL_CHANGESETS = "/alfresco/s/api/solr/aclchangesets";
 
@@ -64,7 +64,6 @@ public class AlfrescoWebscriptClient {
     private Client client;
 
 
-
     public AlfrescoWebscriptClient() {
         client = ClientBuilder.newBuilder()
                 .readTimeout(alfrescoReadTimeout, TimeUnit.MILLISECONDS)
@@ -76,7 +75,7 @@ public class AlfrescoWebscriptClient {
         }
     }
 
-    public List<Node> getNodes(List<Long> transactionIds ){
+    public List<Node> getNodes(List<Long> transactionIds) {
 
         String url = getUrl(URL_NODES_TRANSACTION);
 
@@ -90,42 +89,44 @@ public class AlfrescoWebscriptClient {
         return node.getNodes();
     }
 
-    public String getTextContent(Long dbid){
+    public String getTextContent(Long dbid) {
         String url = getUrl(URL_CONTENT);
-        url += "?nodeId="+dbid;
+        url += "?nodeId=" + dbid;
         return client.target(url)
                 .request(MediaType.TEXT_PLAIN)
                 .get().readEntity(String.class);
     }
 
-    public NodeMetadatas getNodeMetadata(GetNodeMetadataParam param){
-        return this.getNodeMetadata(param,false);
+    public NodeMetadatas getNodeMetadata(GetNodeMetadataParam param) {
+        return this.getNodeMetadata(param, false);
     }
-    public NodeMetadatas getNodeMetadata(GetNodeMetadataParam param, boolean debug){
+
+    public NodeMetadatas getNodeMetadata(GetNodeMetadataParam param, boolean debug) {
         String url = getUrl(URL_NODE_METADATA);
         Response resp = client.target(url)
                 .request(MediaType.APPLICATION_JSON)
                 .post(Entity.json(param));
 
-        if(debug){
+        if (debug) {
             String valueAsString = resp.readEntity(String.class);
             logger.error("problems with node(s):" + valueAsString);
             return null;
-        }else{
+        } else {
             try {
                 NodeMetadatas nmds = resp.readEntity(NodeMetadatas.class);
                 return nmds;
-            }catch(ResponseProcessingException e){
+            } catch (ResponseProcessingException e) {
                 resp = client.target(url)
                         .request(MediaType.APPLICATION_JSON)
                         .post(Entity.json(param));
                 String valueAsString = resp.readEntity(String.class);
-                logger.error("problems with node(s):" + valueAsString,e);
+                logger.error("problems with node(s):" + valueAsString, e);
                 return null;
             }
         }
     }
-    public NodeMetadata getNodeMetadataUUID(String uuid){
+
+    public NodeMetadata getNodeMetadataUUID(String uuid) {
         String url = getUrl(URL_NODE_METADATA_UUID.replace(
                 "{{uuid}}", uuid
         ));
@@ -136,30 +137,69 @@ public class AlfrescoWebscriptClient {
         NodeMetadataWrapper data = resp.readEntity(NodeMetadataWrapper.class);
         return data.getNode();
     }
-    public List<NodeMetadata> getNodeMetadata(List<Node> nodes){
+
+    public List<NodeMetadata> getNodeMetadata(List<Node> nodes) {
 
         List<Long> dbnodeids = new ArrayList<>();
-        for(Node node : nodes){
+        for (Node node : nodes) {
+            dbnodeids.add(node.getId());
+        }
+
+        return getNodeMetadataByIds(dbnodeids);
+    }
+
+    private List<NodeMetadata> getNodeMetadataByIds(List<Long> dbNodeIds) {
+        GetNodeMetadataParam getNodeMetadataParam = new GetNodeMetadataParam();
+        getNodeMetadataParam.setNodeIds(dbNodeIds);
+
+        NodeMetadatas nmds = getNodeMetadata(getNodeMetadataParam);
+        if (nmds != null) {
+            return nmds.getNodes();
+        } else return new ArrayList<>();
+    }
+
+    public List<NodeMetadata> getNodeMetadataByAllowedTypes(List<Node> nodes, final List<String> types) {
+
+        List<Long> dbnodeids = new ArrayList<>();
+        for (Node node : nodes) {
             dbnodeids.add(node.getId());
         }
 
         GetNodeMetadataParam getNodeMetadataParam = new GetNodeMetadataParam();
         getNodeMetadataParam.setNodeIds(dbnodeids);
 
-        NodeMetadatas nmds = getNodeMetadata(getNodeMetadataParam);
-        if(nmds != null) {
-            return nmds.getNodes();
-        }else return new ArrayList<>();
+        getNodeMetadataParam.setIncludeType(true);
+        getNodeMetadataParam.setIncludeProperties(false);
+        getNodeMetadataParam.setIncludeAspects(false);
+        getNodeMetadataParam.setIncludeAclId(false);
+        getNodeMetadataParam.setIncludeOwner(false);
+        getNodeMetadataParam.setIncludePaths(false);
+        getNodeMetadataParam.setIncludeParentAssociations(false);
+        getNodeMetadataParam.setIncludeChildAssociations(false);
+        getNodeMetadataParam.setIncludeNodeRef(false);
+        getNodeMetadataParam.setIncludeChildIds(false);
+        getNodeMetadataParam.setIncludeTxnId(false);
 
+        NodeMetadatas nmds = getNodeMetadata(getNodeMetadataParam);
+        if (nmds != null) {
+            return getNodeMetadataByIds(nmds.getNodes()
+                    .stream()
+                    .filter(x -> types.contains(x.getType()))
+                    .map(NodeMetadata::getId)
+                    .collect(Collectors.toList()));
+
+        } else return new ArrayList<>();
     }
-    public List<NodeData> getNodeData(List<NodeMetadata> nodes){
-        if(nodes == null || nodes.size() == 0){
+
+
+    public List<NodeData> getNodeData(List<NodeMetadata> nodes) {
+        if (nodes == null || nodes.size() == 0) {
             return new ArrayList<>();
         }
 
         LinkedHashSet<Long> acls = new LinkedHashSet<>();
-        for(NodeMetadata md : nodes){
-            long aclId =  md.getAclId();
+        for (NodeMetadata md : nodes) {
+            long aclId = md.getAclId();
             acls.add(aclId);
         }
         GetPermissionsParam getPermissionsParam = new GetPermissionsParam();
@@ -171,14 +211,12 @@ public class AlfrescoWebscriptClient {
                 .collect(Collectors.toMap(AccessControlList::getAclId, accessControlList -> accessControlList));
 
         List<NodeData> result = new ArrayList<>();
-        for(NodeMetadata nodeMetadata : nodes){
+        for (NodeMetadata nodeMetadata : nodes) {
 
-            NodeMetadata node = null;
-
-            for(Reader reader : readersACL.getAclsReaders()) {
+            for (Reader reader : readersACL.getAclsReaders()) {
                 if (nodeMetadata.getAclId() == reader.aclId) {
                     NodeData nodeData;
-                    if(nodeMetadata.getType().equals("ccm:collection_proposal")) {
+                    if (nodeMetadata.getType().equals("ccm:collection_proposal")) {
                         NodeDataProposal nodeDataProposal = new NodeDataProposal();
                         String parent = nodeMetadata.getParentAssocs().get(0);
                         Serializable original = nodeMetadata.getProperties().
@@ -186,21 +224,21 @@ public class AlfrescoWebscriptClient {
                                                 "ccm:collection_proposal_target"
                                         )
                                 );
-                        if(parent!=null && original != null) {
+                        if (parent != null && original != null) {
                             // no fulltext for the original will be indexed for the proposal to save on complexity
                             try {
                                 nodeDataProposal.setOriginal(
                                         getNodeDataMinimal(getNodeMetadataUUID(Tools.getUUID((String) original)))
                                 );
-                            } catch(Throwable t) {
-                                logger.info("Could not track original node for proposal " + nodeMetadata.getNodeRef() +", original: " + original, t);
+                            } catch (Throwable t) {
+                                logger.info("Could not track original node for proposal " + nodeMetadata.getNodeRef() + ", original: " + original, t);
                             }
                             try {
                                 nodeDataProposal.setCollection(
                                         getNodeDataMinimal(getNodeMetadataUUID(Tools.getUUID(parent)))
                                 );
-                            }catch(Throwable t) {
-                                logger.info("Could not track parent collection for proposal " + nodeMetadata.getNodeRef() +", parent " + parent, t);
+                            } catch (Throwable t) {
+                                logger.info("Could not track parent collection for proposal " + nodeMetadata.getNodeRef() + ", parent " + parent, t);
                             }
                         } else {
                             logger.warn("Collection proposal has no parent or target: " + nodeMetadata.getNodeRef());
@@ -221,25 +259,25 @@ public class AlfrescoWebscriptClient {
         }
 
 
-        for(NodeData nodeData : result){
-            if(trackContent) {
+        for (NodeData nodeData : result) {
+            if (trackContent) {
                 String fullText = getTextContent(nodeData.getNodeMetadata().getId());
                 if (fullText != null) nodeData.setFullText(fullText);
             }
 
 
             List<String> allowedChildTypes = new ArrayList<>();
-            if("ccm:io".equals(nodeData.getNodeMetadata().getType())){
+            if ("ccm:io".equals(nodeData.getNodeMetadata().getType())) {
                 // io/file -> we allow everything
                 allowedChildTypes.add("ALL");
             } else if ("ccm:map".equals(nodeData.getNodeMetadata().getType())
-                    && nodeData.getNodeMetadata().getAspects().contains("ccm:collection")){
+                    && nodeData.getNodeMetadata().getAspects().contains("ccm:collection")) {
                 // map/folder -> we only allow specific elements relvant for maps
                 allowedChildTypes.add("ccm:collection_proposal");
             }
 
             List<Node> children = new ArrayList<>();
-            if(nodeData.getNodeMetadata().getChildIds() != null) {
+            if (nodeData.getNodeMetadata().getChildIds() != null) {
                 for (Long dbid : nodeData.getNodeMetadata().getChildIds()) {
                     Node childNode = new Node();
                     childNode.setId(dbid);
@@ -247,10 +285,15 @@ public class AlfrescoWebscriptClient {
                 }
 
                 if (children.size() > 0 && allowedChildTypes.size() > 0) {
-                    List<NodeData> childrenFiltered = getNodeData(this.getNodeMetadata(children).stream().filter((node) ->
-                            allowedChildTypes.contains("ALL") || allowedChildTypes.contains(node.getType())
-                    ).collect(Collectors.toList()));
-                    if(childrenFiltered.size() > 0) {
+                    List<NodeMetadata> nodeMetadata;
+                    if (allowedChildTypes.contains("ALL")) {
+                        nodeMetadata = this.getNodeMetadata(children);
+                    } else {
+                        nodeMetadata = this.getNodeMetadataByAllowedTypes(children, allowedChildTypes);
+                    }
+
+                    List<NodeData> childrenFiltered = getNodeData(nodeMetadata);
+                    if (childrenFiltered.size() > 0) {
                         nodeData.getChildren().addAll(childrenFiltered);
                     }
                 }
@@ -276,7 +319,7 @@ public class AlfrescoWebscriptClient {
 
 
         LinkedHashSet<Long> acls = new LinkedHashSet<>();
-        long aclId =  nodeMetadata.getAclId();
+        long aclId = nodeMetadata.getAclId();
         acls.add(aclId);
         GetPermissionsParam getPermissionsParam = new GetPermissionsParam();
         getPermissionsParam.setAclIds(new ArrayList<Long>(acls));
@@ -291,7 +334,7 @@ public class AlfrescoWebscriptClient {
         return data;
     }
 
-    public ReadersACL getReader(GetPermissionsParam param){
+    public ReadersACL getReader(GetPermissionsParam param) {
         String url = getUrl(URL_ACL_READERS);
         ReadersACL readers = client.target(url)
                 .request(MediaType.APPLICATION_JSON)
@@ -300,7 +343,7 @@ public class AlfrescoWebscriptClient {
     }
 
 
-    public Transactions getTransactions(Long minTxnId, Long maxTxnId, Long fromCommitTime, Long toCommitTime, int maxResults){
+    public Transactions getTransactions(Long minTxnId, Long maxTxnId, Long fromCommitTime, Long toCommitTime, int maxResults) {
 
 
         String url = getUrl(URL_TRANSACTIONS);
@@ -309,7 +352,7 @@ public class AlfrescoWebscriptClient {
         String toParam = "maxTxnId";
         Long fromValue = minTxnId;
         Long toValue = maxTxnId;
-        if(fromCommitTime != null && fromCommitTime > -1){
+        if (fromCommitTime != null && fromCommitTime > -1) {
             fromParam = "fromCommitTime";
             toParam = "toCommitTime";
             fromValue = fromCommitTime;
@@ -318,27 +361,27 @@ public class AlfrescoWebscriptClient {
 
         Transactions transactions = client
                 .target(url)
-                .queryParam(fromParam,fromValue)
-                .queryParam(toParam,toValue)
-                .queryParam("maxResults",maxResults)
+                .queryParam(fromParam, fromValue)
+                .queryParam(toParam, toValue)
+                .queryParam("maxResults", maxResults)
                 .request(MediaType.APPLICATION_JSON)
                 .get(Transactions.class);
 
         return transactions;
     }
 
-    public AclChangeSets getAclChangeSets(Long fromId, Long toId, int maxResults){
+    public AclChangeSets getAclChangeSets(Long fromId, Long toId, int maxResults) {
         String url = getUrl(URL_ACL_CHANGESETS);
         AclChangeSets result = client.target(url)
-                .queryParam("fromId",fromId)
-                .queryParam("toId",toId)
-                .queryParam("maxResults",maxResults)
+                .queryParam("fromId", fromId)
+                .queryParam("toId", toId)
+                .queryParam("maxResults", maxResults)
                 .request(MediaType.APPLICATION_JSON)
                 .get(AclChangeSets.class);
         return result;
     }
 
-    public Acls getAcls(GetAclsParam param){
+    public Acls getAcls(GetAclsParam param) {
         String url = getUrl(URL_ACLS);
         Acls readers = client.target(url)
                 .request(MediaType.APPLICATION_JSON)
@@ -348,7 +391,7 @@ public class AlfrescoWebscriptClient {
     }
 
 
-    public AccessControlLists getAccessControlLists(GetPermissionsParam param){
+    public AccessControlLists getAccessControlLists(GetPermissionsParam param) {
         String url = getUrl(URL_PERMISSIONS);
         AccessControlLists accessControlLists = client.target(url)
                 .request(MediaType.APPLICATION_JSON)
@@ -357,11 +400,7 @@ public class AlfrescoWebscriptClient {
     }
 
 
-
-
-
-
-    private String getUrl(String path){
-        return alfrescoProtocol+"://"+alfrescoHost+":"+alfrescoPort+path;
+    private String getUrl(String path) {
+        return alfrescoProtocol + "://" + alfrescoHost + ":" + alfrescoPort + path;
     }
 }
