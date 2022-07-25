c62f61401895b8b6c0b1b0a58000c6d30a5d1ab2
wollner <wollner@edu-sharing.net>
2022-07-25 11:49:55 +0200

    fix: (DIRTY HACK!!!) adding node to collection causes reindexing collection nodes which can be a large number (10.000 and more)

--- src/main/java/org/edu_sharing/elasticsearch/elasticsearch/client/SearchHitsRunner.java
+++ src/main/java/org/edu_sharing/elasticsearch/elasticsearch/client/SearchHitsRunner.java
@@ -3,11 +3,14 @@ package org.edu_sharing.elasticsearch.elasticsearch.client;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.SearchHits;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
 public abstract class SearchHitsRunner {
+
+    private static final Logger logger = LoggerFactory.getLogger(SearchHitsRunner.class);
 
     protected ElasticsearchClient elasticClient;
     public SearchHitsRunner(ElasticsearchClient elasticClient){
@@ -17,7 +20,12 @@ public abstract class SearchHitsRunner {
     public void run(QueryBuilder queryBuilder)throws IOException {
         this.run(queryBuilder,5);
     }
+
     public void run(QueryBuilder queryBuilder, int pageSize)throws IOException {
+        this.run(queryBuilder,pageSize, null);
+    }
+
+    public void run(QueryBuilder queryBuilder, int pageSize, Integer maxResultsSize)throws IOException {
 
         int page = 0;
         SearchHits searchHits = null;
@@ -26,6 +34,10 @@ public abstract class SearchHitsRunner {
                 page+=pageSize;
             }
             searchHits = elasticClient.search(ElasticsearchClient.INDEX_WORKSPACE, queryBuilder, page, pageSize);
+            if(maxResultsSize != null && searchHits.getTotalHits().value > maxResultsSize){
+                logger.warn("max result size has been reached: found {} of {} allowed", searchHits.getTotalHits().value, maxResultsSize);
+                return;
+            }
             for(SearchHit searchHit : searchHits.getHits()){
                 execute(searchHit);
             }
