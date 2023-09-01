eff88c3e223f3f8c69ddb4e3ecd4c31e6a0d9f25
Daniel Rudolph <rudolph@edu-sharing.net>
2023-09-01 10:02:36 +0200

    fix: fixmissing log partitions size

--- tracker/src/main/java/org/edu_sharing/elasticsearch/tracker/FixMissingTracker.java
+++ tracker/src/main/java/org/edu_sharing/elasticsearch/tracker/FixMissingTracker.java
@@ -94,7 +94,7 @@ public class FixMissingTracker extends TransactionTracker{
         int c = 0;
         int p = 0;
         for(List<Node> partition : partitions){
-            logger.info("indexing main partition " + p + " size:"  + partition.size());
+            logger.info("indexing main partition " + p + " partition size:"  + partition.size() +" partitions size:" +partitions.size());
             c += index(partition);
             p++;
         }
